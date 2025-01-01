085e9fb36d1e4c76579786ff9140937500d486e2
kunal khairnar <kunalkhairnar61@gmail.com>
2025-01-01 19:24:18 +0530

    addFlights component update data between the componnent

--- src/Pages/Admin/BookingDetail.js
+++ src/Pages/Admin/BookingDetail.js
@@ -59,8 +59,8 @@ const BookingDetail = () => {
               </tr>
             </thead>
             <tbody className="bg-white">
-              {bookingFlightData.map((item) => (
-                <tr key={item.userId}>
+              {bookingFlightData.map((item,index) => (
+                <tr key={index}>
                   <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                     <div className="flex items-center">
                       <div className="ml-4">
